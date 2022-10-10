0ac5f46772e65aac5f8fb10128a97881ee7a12d8
John Michael <obligar@live.no>
2022-10-11 00:29:14 +0200

    First Commit

---
+++ android/app/src/main/kotlin/com/example/cabin_checklist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cabin_checklist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cabin_checklist/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HomePage());
+}
